0e4628b6d0e69087c0e2b3c121540d280fb76880
Marek Szuba <mszuba01@gmail.com>
2020-11-01 23:51:06 +0100

    initial commit

---
+++ src/App.jsx
@@ -0,0 +1,9 @@
+import React from "react";
+
+import "./App.scss";
+
+const App = () => {
+  <header>Hellow world!</header>;
+};
+
+export default App;
---
+++ webpack.config.js
@@ -0,0 +1,9 @@
+const { merge } = require("webpack-merge");
+
+const commonConfiguration = require("./webpack/common");
+
+module.exports = (_env, { mode }) => {
+  const properConfig = require(`./webpack/${mode}`);
+  const mergedConfig = merge(commonConfiguration, properConfig);
+  return mergedConfig;
+};
