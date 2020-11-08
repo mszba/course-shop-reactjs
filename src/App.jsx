0b6cdb9f97e682ad58dad59fdcb8a352bd816c89
Marek Szuba <mszuba01@gmail.com>
2020-11-08 20:55:32 +0100

    feat: User login/logout in app

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,15 @@
 import React from "react";
 
+import Header from "./components/Header";
+import StoreProvider from "./store/StoreProvider";
+
 import "./App.scss";
 
 const App = () => {
   return (
-    <>
-      <header>Hellow world!</header>
-    </>
+    <StoreProvider>
+      <Header />
+    </StoreProvider>
   );
 };
 
