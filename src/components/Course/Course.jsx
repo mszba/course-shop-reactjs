fa0e4c7a9dd2c9e421ce45905058a42d28a5a196
Marek Szuba <mszuba01@gmail.com>
2020-11-13 00:24:05 +0100

    feat: Buy button for courses

--- src/components/Course/Course.jsx
+++ src/components/Course/Course.jsx
@@ -1,20 +1,49 @@
-import React from "react";
-import bemCssModules from "bem-css-modules";
+import React, { useContext } from 'react';
+import { useHistory } from 'react-router-dom';
+import bemCssModules from 'bem-css-modules';
 
-import { default as CourseStyle } from "./Course.module.scss";
+import { StoreContext } from '../../store/StoreProvider';
+
+import { default as CourseStyle } from './Course.module.scss';
+import request from '../../helpers/request';
 
 const style = bemCssModules(CourseStyle);
 
-const Course = ({ authors, img, price, title }) => {
-  const allAuthors = authors.join(", ");
+const Course = ({ authors, id, img, isUserContext = false, price, title }) => {
+  const { user, setUser } = useContext(StoreContext);
+  const history = useHistory();
+
+  const allAuthors = authors.join(', ');
+  const isUserLogged = Boolean(user);
+
+  const handleOnClick = async () => {
+    try {
+      const { data, status } = await request.patch('/users', {
+        login: user.login,
+        courseId: id,
+      });
+
+      if (status === 202) {
+        setUser(data.user);
+        history.push('/my-courses');
+      }
+    } catch (error) {
+      console.warn(error);
+    }
+  };
+
+  const shouldBeBuyButtonVisible = isUserLogged && !isUserContext;
 
   return (
     <li>
       <article className={style()}>
-        <h3 className={style("title")}></h3>
-        <img src={img} alt={title} className={style("image")} />
-        <p className={style("price")}>{`Koszt kursu: ${price}zł`}</p>
-        <p className={style("authors")}>{`Autorzy kursu: ${allAuthors}`}</p>
+        <h3 className={style('title')}></h3>
+        <img src={img} alt={title} className={style('image')} />
+        <p className={style('price')}>{`Koszt kursu: ${price}zł`}</p>
+        <p className={style('authors')}>{`Autorzy kursu: ${allAuthors}`}</p>
+        {shouldBeBuyButtonVisible && (
+          <button onClick={handleOnClick}>Kup ten kurs</button>
+        )}
       </article>
     </li>
   );
